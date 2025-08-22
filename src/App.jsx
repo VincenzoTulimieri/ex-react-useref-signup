397a127f00be1ee4594959d799301c7534caa516
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-08-22 14:16:20 +0200

    fix: textarea e button

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,24 @@ import { useState } from 'react'
 
 
 function App() {
-  
+
   return (
     <>
       <h1>Inserisci i tuoi dati</h1>
       <form action="" className='form-container'>
-        <input type="text" placeholder='Nome'className='input-size'/>
-        <input type="text" placeholder='Cognome'className='input-size'/>
-        <input type="password" placeholder='Password'className='input-size'/>
-        <input type="number" placeholder='Esperienza' className='select-number-size'/>
+        <input type="text" placeholder='Nome' className='input-size' />
+        <input type="text" placeholder='Cognome' className='input-size' />
+        <input type="password" placeholder='Password' className='input-size' />
+        <input type="number" placeholder='Esperienza' className='select-number-size' />
         <select name="" id="" >
           <option value="---" className='select-number-size'>---</option>
           <option value="Full Stack">Full Stack</option>
           <option value="Frontend">Frontend</option>
           <option value="Backend">Backend</option>
         </select>
-        <input type="textarea" placeholder='Descrizione'className='input-size'/>
+        <textarea className="input-size" rows="3" placeholder='Descrizione'></textarea>
         <div className='button-container'>
-            <button type="submit">Invia</button>
+          <button type="submit">Invia</button>
         </div>
       </form>
     </>
