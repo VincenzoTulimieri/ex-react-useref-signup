bfb6202574bf988f9cd931c541bc2f7136fe25fc
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-08-23 21:17:35 +0200

    feat: formattazione documento

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,6 @@ function App() {
   }
 
   function isValid(userName) {
-
     if (!userName) {
       return false
     }
@@ -49,11 +48,10 @@ function App() {
       return false
     }
 
-    const letters = "abcdefghijklmnopqrstuvwxyz0123456789"
+    const lettersAndNumbers = "abcdefghijklmnopqrstuvwxyz0123456789"
     const userNameLetter = userName.toLowerCase().split('')
 
-    return userNameLetter.every(l=> letters.includes(l))
-    
+    return userNameLetter.every(element => lettersAndNumbers.includes(element))
   }
 
 
@@ -63,17 +61,23 @@ function App() {
       <h1>Inserisci i tuoi dati</h1>
       <form onSubmit={sendData} className='form-container'>
         <input type="text" placeholder='Nome e cognome' className='input-size' value={name} onChange={e => { setName(e.target.value) }} />
+
         <input type="text" placeholder='Username' className='input-size' value={userName} onChange={e => { setUserName(e.target.value) }} />
         <strong className={isValid(userName) ? 'green' : 'red'}>{isValid(userName) ? 'Username valido' : 'Username non valido'}</strong>
+
         <input type="password" placeholder='Password' className='input-size' value={password} onChange={e => { setPassword(e.target.value) }} />
+
         <input type="number" placeholder='Esperienza' className='select-number-size' value={experience} onChange={e => { setExperience(e.target.value) }} />
+
         <select name="" id="" value={selectedValue} onChange={e => { setSelectedValue(e.target.value) }}>
           <option value="---" className='select-number-size'>---</option>
           {options.map(option => (
             <option key={option.id} value={option.value}>{option.value}</option>
           ))}
         </select>
+
         <textarea className="input-size" rows="3" placeholder='Descrizione' value={description} onChange={e => { setDescription(e.target.value) }}></textarea>
+
         <div className='button-container'>
           <button type="submit">Invia</button>
         </div>
