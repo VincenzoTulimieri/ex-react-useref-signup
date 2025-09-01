586aad089abb43ad157f221dac4bacf9485ec994
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-01 15:12:16 +0200

    feat: inserito ref negli input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 
 function App() {
-
-  const [name, setName] = useState('')
+  
+  const nameRef = useRef()
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
-  const [experience, setExperience] = useState('')
-  const [selectedValue, setSelectedValue] = useState('')
+  const experienceRef= useRef()
+  const selectedValueRef = useRef()
   const [description, setDescription] = useState('')
 
   const options = [
@@ -19,18 +19,20 @@ function App() {
   function sendData(event) {
     event.preventDefault()
     const formData = {
-      name: name,
+      name: nameRef.current.value,
       userName: userName,
       password: password,
-      experience: experience,
-      selectedValue: selectedValue,
+      experience: experienceRef.current.value,
+      selectedValue: selectedValueRef.current.value,
       description: description,
     };
 
-    if (formData.name === '' || formData.userName === '' || formData.password === '' || formData.selectedValue === '---' || formData.description === '' || formData.experience < 0) {
+    if (formData.name === '' || formData.userName === '' || formData.password === '' || formData.selectedValue === '---' || formData.description === '' || formData.experience === '') {
       console.log('Inserire i dati')
       return
-    } else {
+    } else if (formData.experience < 0 ){
+      console.log('Inserire dati corretti, esperienza negativa')
+    }else {
       console.log(formData)
     }
 
@@ -57,24 +59,32 @@ function App() {
     return letters && numbers && symbols
   }
 
+  function isValidDescrption(description){
+    const descriptionValid = description.trim()
+    if(descriptionValid.length >= 100 && descriptionValid.length <= 1000){
+      return true
+    }else{
+      return false
+    }
+  }
 
 
   return (
     <>
       <h1>Inserisci i tuoi dati</h1>
       <form onSubmit={sendData} className='form-container'>
-        <input type="text" placeholder='Nome e cognome' className='input-size' value={name} onChange={e => { setName(e.target.value) }} />
+        <input type="text" placeholder='Nome e cognome' className='input-size' ref={nameRef} />
 
         <input type="text" placeholder='Username' className='input-size' value={userName} onChange={e => { setUserName(e.target.value) }} />
         <strong className={isValidUser(userName) ? 'green' : 'red'}>{isValidUser(userName) ? 'Username valido' : 'Username non valido'}</strong>
 
-        <input type="text" placeholder='Password' className='input-size' value={password} onChange={e => { setPassword(e.target.value) }} />
+        <input type="password" placeholder='Password' className='input-size' value={password} onChange={e => { setPassword(e.target.value) }} />
         <strong className={isValidPass(password) ? 'green' : 'red'}>{isValidPass(password) ? 'Password valida' : 'Password non valida'}</strong>
 
         <div className='select-number-container'>
-          <input type="number" placeholder='Esperienza' value={experience} onChange={e => { setExperience(e.target.value) }} />
+          <input type="number" placeholder='Esperienza' ref={experienceRef} />
 
-          <select name="" id="" value={selectedValue} onChange={e => { setSelectedValue(e.target.value) }}>
+          <select name="" id="" ref={selectedValueRef}>
             <option value="---" >---</option>
             {options.map(option => (
               <option key={option.id} value={option.value}>{option.value}</option>
@@ -83,6 +93,7 @@ function App() {
         </div>
 
         <textarea className="input-size" rows="3" placeholder='Descrizione' value={description} onChange={e => { setDescription(e.target.value) }}></textarea>
+        <strong className={isValidDescrption(description) ? 'green' : 'red'}>{isValidDescrption(description) ? 'Testo valido' : 'Testo non valido'}</strong>
 
         <div className='button-container'>
           <button type="submit">Invia</button>
